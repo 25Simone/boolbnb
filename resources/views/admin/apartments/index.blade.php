1ae2e105f5b9da683158149cf1b4cac8c8002ed6
Tommaso <tommaso.macco@gmail.com>
2022-04-13 01:21:01 +0200

    index blade apartment (Debug seeder and controller)

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -6,10 +6,50 @@
         <h4 class="nav-item">
             <a class="nav-link" href="{{route('admin.apartments.create')}}">Create</a>
         </h4>
-        <ul>
-            <li>
-
-            </li>
-        </ul>
+        {{-- <ul>
+            @foreach ($apartments as $apartment)
+                <li>{{ $apartment->title }}</li>
+                <li>{{ $apartment->rooms_number }}</li>
+                <li>{{ $apartment->beds_number }}</li>
+                <li>{{ $apartment->baths_number }}</li>
+                <li>{{ $apartment->guests }}</li>
+                <li>{{ $apartment->squaremeters }}</li>
+                <li>{{ $apartment->longitude }}</li>
+                <li>{{ $apartment->latitude }}</li>
+                <li>{{ $apartment->visible }}</li>
+                <li>{{ $apartment->slug }}</li>
+                <li>{{ $apartment->photo }}</li>
+                <li>{{ $apartment->address }}</li>
+                <li>{{ $apartment->user->name }}</li>
+            @endforeach
+        </ul> --}}
+        <table class="table table-dark">
+            <thead>
+                <tr>
+                    <th class="col">Title</th>
+                    <th class="col">Rooms number</th>
+                    <th class="col">Beds Number</th>
+                    <th class="col">Baths Number</th>
+                    <th class="col">Guest Number</th>
+                    <th class="col">MQ</th>
+                    <th class="col">Adress</th>
+                    <th class="col">Apartment Owner</th>
+                </tr>
+            </thead>
+            <tbody>
+                @foreach ($apartments as $apartment)
+                    <tr>
+                        <th class="row">{{ $apartment->title }}</th>
+                        <td>{{ $apartment->rooms_number }}</td>
+                        <td>{{ $apartment->beds_number }}</td>
+                        <td>{{ $apartment->baths_number }}</td>
+                        <td>{{ $apartment->guests }}</td>
+                        <td>{{ $apartment->squaremeters }}</td>
+                        <td>{{ $apartment->address }}</td>
+                        <td>{{ $apartment->user->name }}</td>
+                    </tr>
+                @endforeach
+            </tbody>
+        </table>
     </div>
 @endsection
