9b51841675e538235ceab9f0700a237a56bf77af
mattia laurenzana <mattilaure@gmail.com>
2022-04-26 09:51:22 +0200

    Add birth_date field to create function

--- app/Http/Controllers/Auth/RegisterController.php
+++ app/Http/Controllers/Auth/RegisterController.php
@@ -49,6 +49,7 @@ class RegisterController extends Controller
      */
     protected function validator(array $data)
     {
+        
         return Validator::make($data, [
             'name' => ['required', 'string', 'max:255'],
             'email' => ['required', 'string', 'email', 'max:255', 'unique:users'],
@@ -68,7 +69,10 @@ class RegisterController extends Controller
         return User::create([
             'name' => $data['name'],
             'email' => $data['email'],
+            'birth_date' => $data['birth_date'],
             'password' => Hash::make($data['password']),
+            
         ]);
+     
     }
 }
