e7fe30c40dc30ca54ee02225a9e7f68b30db612a
25Simone <dantsimone111@gmail.com>
2022-04-26 09:39:51 +0200

    add additional services in card component

--- resources/js/components/TheCard.vue
+++ resources/js/components/TheCard.vue
@@ -2,15 +2,27 @@
     <div class="px-4">
         <div class="container-fluid py-3 border-top">
             <div class="row">
-                <div class="image-container">
+                <div class="image-container col-2">
                     <img :src="apartment.photo" :alt="apartment.title + 'image'">
                 </div>
-                <div class="ad-content px-5">
+                <div class="ad-content px-5 col-7">
                     <h2>{{ apartment.title }}</h2>
                     <div> <i class="fas fa-map-marker-alt my-3"></i> {{ apartment.address }} </div>
-                    <span class="me-3"> <i class="fas fa-home"></i> {{ apartment.rooms_number }} rooms </span>
-                    <span class="me-3"> <i class="fas fa-bed"></i> {{ apartment.beds_number }} beds </span>
-                    <span class="me-3"> <i class="fas fa-bath"></i> {{ apartment.baths_number }} baths </span>
+                    <div>
+                        <span class="me-3"> <i class="fas fa-home"></i> {{ apartment.rooms_number }} rooms </span>
+                        <span class="me-3"> <i class="fas fa-bed"></i> {{ apartment.beds_number }} beds </span>
+                        <span class="me-3"> <i class="fas fa-bath"></i> {{ apartment.baths_number }} baths </span>
+                    </div>
+                    <div>
+                        <h5 class="pt-3 pb-2">Additional Services</h5>
+                        <span
+                        class="mx-2"
+                        v-for="(service, i) in apartment.additional_services"
+                        :key="i"
+                        >
+                            {{ service.name }}
+                        </span>
+                    </div>
                 </div>
             </div>
         </div>
@@ -30,7 +42,7 @@ export default {
     .row {
         height: 23vh;
         .image-container {
-            width: 20%;
+            // width: 20%;
             height: 100%;
             img {
                 width: 100%;
@@ -41,7 +53,7 @@ export default {
         }
         .ad-content {
             margin-left: 30px;
-            width: 70%;
+            // width: 70%;
             i {
                 color: #ff385c;
             }
