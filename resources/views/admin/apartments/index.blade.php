6f3539935b6782c776ae4743d133f52ce0150ba6
benedettagns <benedetta.genise01@gmail.com>
2022-04-27 11:39:31 +0200

    refactoring style index.blade

--- resources/views/admin/apartments/index.blade.php
+++ resources/views/admin/apartments/index.blade.php
@@ -1,42 +1,41 @@
 @extends('layouts.app')
 
 @section('content')
-    <div class="container">
-        <h1>APARTMENTS</h1>
-        <h4 class="nav-item">
-            <a class="nav-link" href="{{route('admin.apartments.create')}}">Create</a>
-        </h4>
-        <table class="table table-dark">
-            <thead>
-                <tr>
-                    <th class="col">Title</th>
-                    <th class="col">Rooms number</th>
-                    <th class="col">Beds Number</th>
-                    <th class="col">Baths Number</th>
-                    <th class="col">Guest Number</th>
-                    <th class="col">MQ</th>
-                    <th class="col">Adress</th>
-                    <th class="col">Apartment Owner</th>
-                    <th class="col">Show</th>
-                    <th class="col">Edit</th>
-                </tr>
-            </thead>
-            <tbody>
-                @foreach ($apartments as $apartment)
-                    <tr>
-                        <td>{{ $apartment->title }}</td>
-                        <td>{{ $apartment->rooms_number }}</td>
-                        <td>{{ $apartment->beds_number }}</td>
-                        <td>{{ $apartment->baths_number }}</td>
-                        <td>{{ $apartment->guests }}</td>
-                        <td>{{ $apartment->squaremeters }}</td>
-                        <td>{{ $apartment->address }}</td>
-                        <td>{{ $apartment->user->name }}</td>
-                        <td><a class="btn btn-primary" href="{{route('admin.apartments.show', $apartment->slug)}}">Show</a></td>
-                        <td><a class="btn btn-primary" href="{{route('admin.apartments.edit', $apartment->id)}}">Edit</a></td>
-                    </tr>
-                @endforeach
-            </tbody>
-        </table>
+    <div class="container-fluid">
+        <div class="header-container">
+            <h2>MyAPARTMENTS</h2>
+            <div class="flex">
+                <button class="btn btn-light">
+                    <a class="btn-create" href="{{route('admin.apartments.create')}}">Create</a>
+                </button>
+            </div>
+            
+        </div>
+
+        <div class="card-group">
+            @foreach ($apartments as $apartment)
+                <div class="card" style="width: 18rem;">
+
+                    <div class="card-body">
+                        <h4 class="card-title">{{ $apartment->title }}</h4>
+                        <div>
+                            <span>Rooms:{{ $apartment->rooms_number }}</span>
+                            <span>Beds:{{ $apartment->beds_number }}</span>
+                            <span>Bath:{{ $apartment->baths_number }}</span>
+                        </div>
+                        <div>
+                            <span>Guest:{{ $apartment->guests }}</span>
+                            <span>MQ:{{ $apartment->squaremeters }}</span>
+                        </div>
+                        <p>Address:{{ $apartment->address }}</p>
+                        <p>Host:{{ $apartment->user->name }}</p>
+                    </div>
+                    <div class="card-footer">
+                        <a class="btn btn-light" href="{{route('admin.apartments.show', $apartment->slug)}}">Show</a>
+                        <a class="btn btn-light" href="{{route('admin.apartments.edit', $apartment->id)}}">Edit</a>
+                    </div>
+                </div>
+            @endforeach
+        </div>
     </div>
 @endsection
