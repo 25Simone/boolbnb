c64354fc0b4953e20d4d083beac9ef337614e74a
25Simone <dantsimone111@gmail.com>
2022-04-28 15:00:22 +0200

    fix edit page layout

--- resources/views/admin/apartments/edit.blade.php
+++ resources/views/admin/apartments/edit.blade.php
@@ -1,126 +1,143 @@
 @extends('layouts.app')
 
 @section('content')
-<div class="container">
-    <h1 class="display-3 text-center">Edit Your Apartment</h1>
-    <form action="{{ route('admin.apartments.update', $apartment) }}" method="post" enctype="multipart/form-data">
-        @csrf
-        @method("put")
-
-        {{-- TITLE SECTION  --}}
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Apartment Title *</label>
-  
-            <input placeholder="Insert your title apartment" type="text" name="title" class="form-control @error('title') is-invalid @enderror" value="{{ old('title', $apartment->title) }}">
-            @error("title")
-              <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
-          </div>
-
-        {{-- ROOMS NUMBER SECTION --}}
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Apartment Rooms *</label>
-  
-            <input placeholder="Insert the number of your rooms" type="number" value="{{ old('rooms_number', $apartment->rooms_number) }}" name="rooms_number" class="form-control  @error('rooms_number') is-invalid @enderror" min='0'>
-              @error("rooms_number")
-                <div class="invalid-feedback">{{ $message }}</div>
-              @enderror
-        </div>
-
-        {{-- BEDS NUMBER SECTION --}}
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Apartment Beds *</label>
-  
-            <input placeholder="Insert the number of your beds" type="number" value="{{ old('beds_number', $apartment->beds_number) }}" name="beds_number" class="form-control  @error('beds_number') is-invalid @enderror" min='0'>
-            @error('beds_number')
-              <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
-        </div>
-
-        {{-- BATHS NUMBER SECTION --}}
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Apartment Baths *</label>
-  
-            <input placeholder="Insert the number of your baths" type="number" value="{{ old('baths_number', $apartment->baths_number) }}" name="baths_number" class="form-control  @error('baths_number') is-invalid @enderror" min='0'>
-            @error('baths_number')
-              <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
-        </div>
-        
-        {{-- GUESTS SECTION  --}}
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Apartment Guests *</label>
-  
-            <input placeholder="Insert the number that the apartment can accommodate" type="number" value="{{ old('guests', $apartment->guests) }}" name="guests" class="form-control  @error('guests') is-invalid @enderror" >
-              @error('guests')
-                <div class="invalid-feedback">{{ $message }}</div>
-              @enderror
-        </div>
-
-        {{-- SQUAREMETERES SECTION --}}
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Apartment Squaremeters</label>
-  
-            <input placeholder="Insert the number of squaremeters of your apartment" type="text" value="{{ old('squaremeters', $apartment->squaremeters) }}" name="squaremeters" class="form-control  @error('squaremeters') is-invalid @enderror">
-              @error('squaremeters')
-                <div class="invalid-feedback">{{ $message }}</div>
-              @enderror
-        </div>
-
-        {{-- ADRESS SECTION --}}
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Apartment Address *</label>
-  
-            <input autocomplete=off id="address_input" placeholder="Via Roma 1, 20099 " type="text" value="{{ old('address', $apartment->address) }}" name="address" class="form-control  @error('address') is-invalid @enderror">
-            <div id="suggestedAddresses"></div>
-            @error('address')
-              <div class="invalid-feedback">{{ $message }}</div>
-            @enderror
-        </div>
-
-        {{-- IMAGE UPLOAD SECTION --}}
-        <div class="mb-3">
-            <label for="exampleInputEmail1" class="form-label">Add photo</label>
-  
-            <input  type="file" name="photo" class="form-control @error('photo') is-invalid @enderror">
-              @error('photo')
-                <div class="invalid-feedback">{{ $message }}</div>
-              @enderror
-              {{-- To fix --}}
-              <div class="image-custom">
-                <img src="{{asset('storage/' . $apartment->photo)}}" alt="">
+{{-- Catch the possible errors --}}
+@if($errors->any())
+  <div class="alert alert-danger">
+    <ul>
+      @foreach($errors->all() as $error)
+        <li>{{ $error }}</li>
+      @endforeach
+    </ul>
+  </div>
+@endif
+<div>
+  <div class="row row-cols-2 m-0 align-items-stretch" id="edit-form-wrapper">
+    <div class="col edit-section-left">
+      <h1 class="display-3 text-center">Edit Your Apartment!</h1>
+    </div>
+    {{-- FORM --}}
+    <div class="container">
+        <form action="{{ route('admin.apartments.update', $apartment) }}" method="post" enctype="multipart/form-data">
+            @csrf
+            @method("put")
+    
+            {{-- TITLE SECTION  --}}
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Apartment Title *</label>
+      
+                <input placeholder="Insert your title apartment" type="text" name="title" class="form-control @error('title') is-invalid @enderror" value="{{ old('title', $apartment->title) }}">
+                @error("title")
+                  <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
               </div>
-          </div>
-
-        {{-- ADDITIONAL SERVICES SECTION --}}
-        <div class="mb-3">
-            <label class="form-label">Additional services *</label>
-            <br>
-            @foreach ($services as $service )
-              <div class="form-check form-check-inline">
-                <label for="service_{{$service->id}}">{{$service->name}}</label>
-                <input
-                {{ ($apartment->additional_services->contains($service) ? 'checked' : '') }}
-                id="service_{{$service->id}}" type="checkbox" class="form-check-input" name="services[]" value="{{$service->id}}">
+    
+            {{-- ROOMS NUMBER SECTION --}}
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Apartment Rooms *</label>
+      
+                <input placeholder="Insert the number of your rooms" type="number" value="{{ old('rooms_number', $apartment->rooms_number) }}" name="rooms_number" class="form-control  @error('rooms_number') is-invalid @enderror" min='0'>
+                  @error("rooms_number")
+                    <div class="invalid-feedback">{{ $message }}</div>
+                  @enderror
+            </div>
+    
+            {{-- BEDS NUMBER SECTION --}}
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Apartment Beds *</label>
+      
+                <input placeholder="Insert the number of your beds" type="number" value="{{ old('beds_number', $apartment->beds_number) }}" name="beds_number" class="form-control  @error('beds_number') is-invalid @enderror" min='0'>
+                @error('beds_number')
+                  <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+    
+            {{-- BATHS NUMBER SECTION --}}
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Apartment Baths *</label>
+      
+                <input placeholder="Insert the number of your baths" type="number" value="{{ old('baths_number', $apartment->baths_number) }}" name="baths_number" class="form-control  @error('baths_number') is-invalid @enderror" min='0'>
+                @error('baths_number')
+                  <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+            
+            {{-- GUESTS SECTION  --}}
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Apartment Guests *</label>
+      
+                <input placeholder="Insert the number that the apartment can accommodate" type="number" value="{{ old('guests', $apartment->guests) }}" name="guests" class="form-control  @error('guests') is-invalid @enderror" >
+                  @error('guests')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                  @enderror
+            </div>
+    
+            {{-- SQUAREMETERES SECTION --}}
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Apartment Squaremeters</label>
+      
+                <input placeholder="Insert the number of squaremeters of your apartment" type="text" value="{{ old('squaremeters', $apartment->squaremeters) }}" name="squaremeters" class="form-control  @error('squaremeters') is-invalid @enderror">
+                  @error('squaremeters')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                  @enderror
+            </div>
+    
+            {{-- ADRESS SECTION --}}
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Apartment Address *</label>
+      
+                <input autocomplete=off id="address_input" placeholder="Via Roma 1, 20099 " type="text" value="{{ old('address', $apartment->address) }}" name="address" class="form-control  @error('address') is-invalid @enderror">
+                <div id="suggestedAddresses"></div>
+                @error('address')
+                  <div class="invalid-feedback">{{ $message }}</div>
+                @enderror
+            </div>
+    
+            {{-- IMAGE UPLOAD SECTION --}}
+            <div class="mb-3">
+                <label for="exampleInputEmail1" class="form-label">Add photo</label>
+      
+                <input  type="file" name="photo" class="form-control @error('photo') is-invalid @enderror">
+                  @error('photo')
+                    <div class="invalid-feedback">{{ $message }}</div>
+                  @enderror
+                  {{-- To fix --}}
+                  <div class="image-custom">
+                    <img src="{{asset('storage/' . $apartment->photo)}}" alt="">
+                  </div>
               </div>
-            @endforeach
-        </div>
-
-        {{-- VISIBLE SECTION --}}
-        <div class="mb-3">
-          <label for="visibleCheckBox">Visible</label>
-          <input type="checkbox" class="form-check-input" name="visible" value=1 {{$apartment->visible ? 'checked' : ''}}>
-        </div>
-
-        {{-- LATITUDE AND LONGITUDE SECTION --}}
-        <div class="d-none">
-          <input type="text" id="latitudeInput" name="latitude" value="{{ old('latitude', $apartment->latitude) }}">
-          <input type="text" id="longitudeInput" name="longitude" value="{{ old('longitude', $apartment->longitude) }}">
-        </div>
-
-        {{-- SUBMIT BUTTON TO STORE --}}
-        <button type="submit" class="btn btn-primary">Update</button>
-      </form>
+    
+            {{-- ADDITIONAL SERVICES SECTION --}}
+            <div class="mb-3">
+                <label class="form-label">Additional services *</label>
+                <br>
+                @foreach ($services as $service )
+                  <div class="form-check form-check-inline">
+                    <label for="service_{{$service->id}}">{{$service->name}}</label>
+                    <input
+                    {{ ($apartment->additional_services->contains($service) ? 'checked' : '') }}
+                    id="service_{{$service->id}}" type="checkbox" class="form-check-input" name="services[]" value="{{$service->id}}">
+                  </div>
+                @endforeach
+            </div>
+    
+            {{-- VISIBLE SECTION --}}
+            <div class="mb-3">
+              <label for="visibleCheckBox">Visible</label>
+              <input type="checkbox" class="form-check-input" name="visible" value=1 {{$apartment->visible ? 'checked' : ''}}>
+            </div>
+    
+            {{-- LATITUDE AND LONGITUDE SECTION --}}
+            <div class="d-none">
+              <input type="text" id="latitudeInput" name="latitude" value="{{ old('latitude', $apartment->latitude) }}">
+              <input type="text" id="longitudeInput" name="longitude" value="{{ old('longitude', $apartment->longitude) }}">
+            </div>
+    
+            {{-- SUBMIT BUTTON TO STORE --}}
+            <button type="submit" class="btn btn-primary">Update</button>
+          </form>
+    </div>
+  </div>
 </div>
 
 <script>
