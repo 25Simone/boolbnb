e1b83e61c1b94b1327d5d9b4daa87cf46eb5cd7f
benedettagns <benedetta.genise01@gmail.com>
2022-04-28 13:57:53 +0200

    add footer

---
+++ resources/js/components/TheFooter.vue
@@ -0,0 +1,93 @@
+<template>
+    <footer>
+        <div class="container">
+            <div class="d-flex justify-content-between pt-5">
+                <div>
+                    <h5>Assistenza</h5>
+                    <p>Centro Assistenza</p>
+                    <p>Informazioni di sicurezza</p>
+                    <p>Opzioni di cancellazione</p>
+                    <p>La nostra risposta all'emergienza COVID-19</p>
+                    <p>Accessibilità per tutti</p>
+                    <p>Segnala problemi nel quartiere</p>
+                </div>
+                <div>
+                    <h5>Community</h5>
+                    <p>Airbnb.org: un rifugio sicuro</p>
+                    <p>Supporta i rifugiati afghani</p>
+                    <p>Contro la discriminazione</p>
+                </div>
+                <div>
+                    <h5>Ospitare</h5>
+                    <p>Prova ad ospitare</p>
+                    <p>AirCover per gli ospiti</p>
+                    <p>Esplora le risorse per host</p>
+                    <p>Vai al forum della community</p>
+                    <p>Come ospitare responsabilmente</p>
+                </div>
+                <div>
+                    <h5>Informazioni</h5>
+                    <p>Newsroom</p>
+                    <p>Scopri le nuove funzionalità</p>
+                    <p>Lettera dai nostri fondatori</p>
+                    <p>Opportunità di lavoro</p>
+                    <p>Investitori</p>
+                    <p>BoolBnB Luxe</p>
+                </div>
+            </div>
+
+            <div class="info-box mt-5 pt-3 pb-3">
+                <div class="copy-box">
+                    <span>© 2022 BoolBnB, Inc.</span>
+                    <span>Privacy</span>
+                    <span>Termini</span>
+                    <span>Mappa del sito</span>
+                    <span>Dettagli dell'azienda</span> 
+                </div>
+                <div class="logo-box">
+                    <span>Italiano(IT)</span>
+                    <span>€ EUR</span>
+                </div>
+            </div>   
+        </div>    
+    </footer>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+footer {
+    background-color: whitesmoke ;
+
+    h5 {
+        font-weight: 900;
+    }
+    p {
+        cursor: pointer;
+        font-size: 13px ;
+        &:hover {
+            text-decoration: underline;
+        }
+    }
+
+    .info-box {
+        border-top: 1px rgb(182, 182, 182) solid ;
+
+        .copy-box {
+            float: left;
+        }
+        
+        .logo-box {
+            float: right;
+        }
+
+        span {
+            padding-left: 10px;
+        }
+    }
+}
+</style>
