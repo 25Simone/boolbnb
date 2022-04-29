13d54237f8fc015261cd8693d4f9e91aa000a2af
benedettagns <benedetta.genise01@gmail.com>
2022-04-29 11:40:59 +0200

    test home

--- resources/js/pages/Home.vue
+++ resources/js/pages/Home.vue
@@ -1,5 +1,35 @@
 <template>
-    <div> <h1>Sono la Home Page</h1> Sono la Home Page</div>
+    <main class="container-fluid bg">
+        <div class="container">
+            <div class="d-flex justify-content-center">
+                <div class="jumbotron">
+                    <h1>Affitta casa dei tuoi sogni</h1>
+                </div>
+            </div>
+
+            <div class="d-flex justify-content-center">
+                <div class="box-img">
+                    ciao
+                </div>
+            </div>
+        
+
+            <div class="ispirazione-container">
+                <h2>Ispirazione per i tuoi viaggi</h2>
+                <div class="row d-flex">
+                    <div class="col card">
+                        <div class="white-box"></div>
+                    </div>
+                    <div class="col card">
+                        <div class="white-box"></div>
+                    </div>
+                    <div class="col card">
+                        <div class="white-box"></div>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </main>
 </template>
 
 <script>
@@ -9,5 +39,41 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+main {
 
+    .bg {
+        background-color: black;
+    }
+
+    .jumbotron {
+        width: 1200px;
+        height: 400px;
+        background-color: #fa4f55;
+        margin-top: 60px ;
+        margin-bottom: 30px;
+        border-radius: 40px;
+
+        h1 {
+            color: white;
+            text-align: center;
+            padding-top: 150px;
+            font-size: 60px;
+        }
+    }
+
+    .box-img {
+        width: 1200px;
+        height: 700px;
+        background-color: white;
+        border-radius: 40px;
+        margin-top: 70px ;
+    }
+
+    .ispirazione-container {
+        margin-top: 80px;
+        h2 {
+            color:white;
+        }
+    }
+}
 </style>
